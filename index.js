7ec59822594aa47bd76d9298aa922d17621bfff3
jesse keane <me@jessekeane.me>
2013-09-27 09:08:54 -0700

    bleh. lets think about this

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 module.exports = merge
 
 function merge() {
-  var args = [].slice.call(arguments)
-  if (!args.length) return {}
-  if (args.length == 1) return args[0]
+  var args = [].slice.call(arguments),
+      l = args.length
+  if (!l) return {}
+  if (l == 1) return args[0]
+  return parseObjects(args)
+}
+
+function parseObjects(objs) {
+  var result = {},
+      keys = []
+  for (var i = 0, l = objs.length; i < l; ++i) {
+    keys = keys.concat(Object.keys(objs[i]))
+  }
+  keys = keys.filter(function (k, idx, arr) { return arr.lastIndexOf(k) === idx })
+
 }
