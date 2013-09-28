da8d8586b2d75c6e6709fbb472dc241e5129b4e1
jesse keane <me@jessekeane.me>
2013-09-27 19:42:00 -0700

    few more tests

--- index.js
+++ index.js
@@ -17,13 +17,18 @@ function parseObjects(objs) {
   keys = keys.filter(function (k, idx, arr) { return arr.lastIndexOf(k) === idx })
   for (var k = 0, f = keys.length; k < f; ++k) {
     var keyResult = [],
-        attr = keys[k]
+        attr = keys[k],
+        convert = false
     objs.map(function (el) {
       if (el.hasOwnProperty(attr)) {
-        if (el[attr] instanceof Array) return keyResult = keyResult.concat(el[attr])
+        if (el[attr] instanceof Array) {
+          keyResult = keyResult.concat(el[attr])
+          return convert = true
+        }
         keyResult.push(el[attr])
       }
     })
+    if (keyResult.length == 1 && !convert) keyResult = keyResult[0]
     result[attr] = keyResult
   }
   return result
--- test/test.js
+++ test/test.js
@@ -4,3 +4,4 @@ var assert = require('assert'),
 assert.deepEqual(objm(), {})
 assert.deepEqual(objm({ hey: 'woo' }), { hey: 'woo' })
 assert.deepEqual(objm({ hey: 'woo' }, { hey: 'wee' }), { hey: ['woo', 'wee'] })
+assert.deepEqual(objm({ a: [1, 2] }, { a: [3, 4], b: 0 }), { a: [1, 2, 3, 4], b: 0 })
