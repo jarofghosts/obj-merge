b5fa8423de2e43ea49b8f0befc87c0253b7e8154
jesse keane <me@jessekeane.me>
2013-09-27 19:36:04 -0700

    i have a feeling this isnt optimal..

--- index.js
+++ index.js
@@ -15,5 +15,16 @@ function parseObjects(objs) {
     keys = keys.concat(Object.keys(objs[i]))
   }
   keys = keys.filter(function (k, idx, arr) { return arr.lastIndexOf(k) === idx })
-
+  for (var k = 0, f = keys.length; k < f; ++k) {
+    var keyResult = [],
+        attr = keys[k]
+    objs.map(function (el) {
+      if (el.hasOwnProperty(attr)) {
+        if (el[attr] instanceof Array) return keyResult = keyResult.concat(el[attr])
+        keyResult.push(el[attr])
+      }
+    })
+    result[attr] = keyResult
+  }
+  return result
 }
