32226c597826952bd87d8852b8c5af77e6bdf9fb
jesse keane <me@jessekeane.me>
2013-12-07 12:29:20 -0800

    minor change

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ module.exports = merge
 function merge() {
   var objs = [].slice.call(arguments),
       l = objs.length
+
   if (!l) return {}
-  if (l == 1) return objs[0]
+  if (l === 1) return objs[0]
+
   var result = {},
       keys = []
-  objs.map(function (o) {
-    keys = keys.concat(Object.keys(o))
-  })
+
+  for (var i = 0; i < l; ++i) {
+    keys = keys.concat(Object.keys(objs[i]))
+  }
+
   keys = keys.filter(function (k, idx, arr) { return arr.lastIndexOf(k) === idx })
+
   for (var k = 0, f = keys.length; k < f; ++k) {
     var keyResult = [],
         attr = keys[k],
         convert = false
+
     objs.map(function (el) {
       if (el.hasOwnProperty(attr)) {
         if (Array.isArray(el[attr])) {
@@ -24,8 +30,10 @@ function merge() {
         keyResult.push(el[attr])
       }
     })
+
     if (keyResult.length == 1 && !convert) keyResult = keyResult[0]
     result[attr] = keyResult
   }
+
   return result
 }
