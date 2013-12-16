c0a3a63f33da33a80a8729f59425cb664e7d27e8
jesse keane <me@jessekeane.me>
2013-12-15 19:48:55 -0800

    minor refactor

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ function merge() {
     keys = keys.concat(Object.keys(objs[i]))
   }
 
-  keys = keys.filter(function (k, idx, arr) { return arr.lastIndexOf(k) === idx })
+  keys = keys.filter(function (k, idx, arr) {
+    return arr.lastIndexOf(k) === idx
+  })
 
   for (var k = 0, f = keys.length; k < f; ++k) {
     var keyResult = [],
@@ -22,16 +24,14 @@ function merge() {
         convert = false
 
     objs.map(function (el) {
-      if (el.hasOwnProperty(attr)) {
-        if (Array.isArray(el[attr])) {
-          keyResult = keyResult.concat(el[attr])
-          return convert = true
-        }
-        keyResult.push(el[attr])
-      }
+      if (!el.hasOwnProperty(attr)) return
+      if (!Array.isArray(el[attr])) return keyResult.push(el[attr])
+
+      keyResult = keyResult.concat(el[attr])
+      convert = true
     })
 
-    if (keyResult.length == 1 && !convert) keyResult = keyResult[0]
+    if (keyResult.length === 1 && !convert) keyResult = keyResult[0]
     result[attr] = keyResult
   }
 
