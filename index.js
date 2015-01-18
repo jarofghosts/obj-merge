31077cf582668c73d343195aae889de5294bff49
Jacob Tipp <jacobtipp@gmail.com>
2015-01-17 16:24:58 -0800

    made some style changes

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 module.exports = merge
 
 function merge() {
-  var objs = [].slice.call(arguments),
-      len = objs.length
+  var objs = [].slice.call(arguments)
+    , len = objs.length
 
   if(!len) return {}
   if(len === 1) return objs.shift()
 
-  function mergeObjects(curr, next) {
-    var keys = Object.keys(curr),
-        key
+  return objs.reduce(mergeObjects)
 
-    for(var i = 0, len = keys.length; i < len; ++i) {
+  function mergeObjects(curr, next) {
+    var keys = Object.keys(curr)
+      , key
+
+    for(var i = 0,len = keys.length; i < len; ++i) {
       key = keys[i]
-      if(next.hasOwnProperty(key)) 
+      if(next.hasOwnProperty(key)) {
         next[key] = [].concat(curr[key]).concat(next[key])
-      else 
+      }
+      else {
         next[key] = curr[key]
+      }
     }
+
     return next
   }
-
-  return objs.reduce(mergeObjects)
 }
