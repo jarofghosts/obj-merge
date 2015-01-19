b133a868527ee3636d808fb7e179cbd2c25dff7c
Jacob Tipp <jacobtipp@gmail.com>
2015-01-19 07:26:40 -0800

    refactored reduce to not mutate original objects

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ function merge() {
   if(!len) return {}
   if(len === 1) return objs.shift()
 
-  return objs.reduce(mergeObjects)
+  return objs.reduce(mergeObjects,{})
 
   function mergeObjects(curr, next) {
-    var keys = Object.keys(curr)
+    var keys = Object.keys(next)
       , key
 
     for(var i = 0,len = keys.length; i < len; ++i) {
       key = keys[i]
-      if(next.hasOwnProperty(key)) {
-        next[key] = [].concat(curr[key]).concat(next[key])
+      if(curr.hasOwnProperty(key)) {
+        curr[key] = [].concat(curr[key]).concat(next[key])
       } else {
-        next[key] = curr[key]
+        curr[key] = next[key]
       }
     }
 
-    return next
+    return curr
   }
 }
